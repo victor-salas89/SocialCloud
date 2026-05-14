379ca61a5e6c6aa99c055e96e85414bf13f2ae3f
Diego alejandro Gonzalez <dia.gonzalezb@duocuc.cl>
2026-05-14 01:55:22 -0400

    Se agrega condigo a clase UpdateUserRequest

--- ms-servicio-usuario/ms-servicio-usuario/src/main/java/com/socialcloud/msserviciousuario/dto/UpdateUserRequest.java
+++ ms-servicio-usuario/ms-servicio-usuario/src/main/java/com/socialcloud/msserviciousuario/dto/UpdateUserRequest.java
@@ -1,4 +1,12 @@
 package com.socialcloud.msserviciousuario.dto;
 
+import lombok.Data;
+
+@Data
 public class UpdateUserRequest {
+
+    private String fullName;
+    private String username;
+    private String bio;
+    private String avatarUrl;
 }
