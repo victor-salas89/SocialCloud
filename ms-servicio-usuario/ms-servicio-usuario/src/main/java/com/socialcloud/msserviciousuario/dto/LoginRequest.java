40283c690dc3c8c26f17d4e54db4892a70fd9038
Victor <vi.salasa@duocuc.cl>
2026-05-14 01:53:44 -0400

    Se agrega codigos a clase LoginRequest del DTO

--- ms-servicio-usuario/ms-servicio-usuario/src/main/java/com/socialcloud/msserviciousuario/dto/LoginRequest.java
+++ ms-servicio-usuario/ms-servicio-usuario/src/main/java/com/socialcloud/msserviciousuario/dto/LoginRequest.java
@@ -1,4 +1,16 @@
 package com.socialcloud.msserviciousuario.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class LoginRequest {
+
+    @Email(message = "El correo debe tener formato válido")
+    @NotBlank(message = "El correo es obligatorio")
+    private String email;
+
+    @NotBlank(message = "La contraseña es obligatoria")
+    private String password;
 }
