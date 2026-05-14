73221647085a069b3ac1b00830c803e2992917db
Diego alejandro Gonzalez <dia.gonzalezb@duocuc.cl>
2026-05-14 01:50:47 -0400

    Se agrega condigo a clase Repository

--- ms-servicio-usuario/ms-servicio-usuario/src/main/java/com/socialcloud/msserviciousuario/repository/UserRepository.java
+++ ms-servicio-usuario/ms-servicio-usuario/src/main/java/com/socialcloud/msserviciousuario/repository/UserRepository.java
@@ -1,4 +1,17 @@
 package com.socialcloud.msserviciousuario.repository;
 
-public interface UserRepository {
+import com.socialcloud.msserviciousuario.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByUsername(String username);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
 }
