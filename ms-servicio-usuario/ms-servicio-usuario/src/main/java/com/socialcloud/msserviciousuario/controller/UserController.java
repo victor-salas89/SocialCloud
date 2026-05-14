7f3490224f9f13a02b617a12bcc74fe0493edde5
Victor <vi.salasa@duocuc.cl>
2026-05-14 02:18:32 -0400

    Se agregan codigos a clase UserController del controller

--- ms-servicio-usuario/ms-servicio-usuario/src/main/java/com/socialcloud/msserviciousuario/controller/UserController.java
+++ ms-servicio-usuario/ms-servicio-usuario/src/main/java/com/socialcloud/msserviciousuario/controller/UserController.java
@@ -1,4 +1,50 @@
 package com.socialcloud.msserviciousuario.controller;
 
+import com.socialcloud.msserviciousuario.dto.UpdateUserRequest;
+import com.socialcloud.msserviciousuario.dto.UserResponse;
+import com.socialcloud.msserviciousuario.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/me")
+    public UserResponse getMyProfile(Authentication authentication) {
+        String email = authentication.getName();
+        return userService.getMyProfile(email);
+    }
+
+    @GetMapping
+    public List<UserResponse> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponse getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PutMapping("/{id}")
+    public UserResponse updateUser(
+            @PathVariable Long id,
+            @RequestBody UpdateUserRequest request
+    ) {
+        return userService.updateUser(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
 }
