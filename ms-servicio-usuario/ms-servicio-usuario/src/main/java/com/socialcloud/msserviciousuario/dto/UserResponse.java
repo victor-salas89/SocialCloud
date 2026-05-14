3372b4d50cce8931b537f6695bff1da20742301a
Victor <vi.salasa@duocuc.cl>
2026-05-14 01:55:23 -0400

    Se agrega codigos a clase UserResponse del DTO

--- ms-servicio-usuario/ms-servicio-usuario/src/main/java/com/socialcloud/msserviciousuario/dto/UserResponse.java
+++ ms-servicio-usuario/ms-servicio-usuario/src/main/java/com/socialcloud/msserviciousuario/dto/UserResponse.java
@@ -1,4 +1,21 @@
 package com.socialcloud.msserviciousuario.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class UserResponse {
+
+    private Long id;
+    private String fullName;
+    private String email;
+    private String username;
+    private String bio;
+    private String avatarUrl;
+    private String role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
